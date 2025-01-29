a250379b66cf615c1316114f7261b8e231915843
Valerio Pinsone <valerio.pinsone@gmail.com>
2025-01-29 23:05:48 +0100

    cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const ModbusRTU = require("modbus-serial");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 app.use(express.json()); 
 
 async function readRegister(ip, plcPort, unitId, register) {
